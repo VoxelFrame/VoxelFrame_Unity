29a1bd16cede607818b14d80c9466ebd6f416c3c
ActivePeter <1020401660@qq.com>
2021-01-28 15:12:04 +0800

    hi

---
+++ server/VoxelServer_CSharp/VoxelServer/Game/PlayerPart/PlayerBaseData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoxelServer.Game.PlayerPart
+{
+    class PlayerBaseData
+    {
+        public int userId;
+        public string userName;
+        public string password;
+    }
+}
